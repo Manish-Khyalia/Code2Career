5e77500920e7399377d3460e569db888f5918c1d
Manish-Khyaliaclear <manishkhyalia2017@gmail.com>
2026-07-10 16:22:21 +0530

    Configure Railway server port

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://code2career-production.up.railway.app";
 
 export const login = async (loginData) => {
 
